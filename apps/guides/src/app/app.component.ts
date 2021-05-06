d85c93f6ad420777dd51a98c377fb310d2c56b07
Supamiu <contact@flavien-normand.fr>
2021-05-06 19:05:50 +0200

    initial commit

---
+++ apps/guides/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'teamcraft-guides-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+})
+export class AppComponent {
+  title = 'guides';
+}
