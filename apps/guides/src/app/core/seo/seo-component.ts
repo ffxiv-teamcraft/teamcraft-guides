5934a0a5742d0429475a044c6b326153180ecccf
Supamiu <contact@flavien-normand.fr>
2021-05-13 10:52:04 +0200

    feat(core): betta meta tags, SEO everywhere !

---
+++ apps/guides/src/app/core/seo/seo-component.ts
@@ -0,0 +1,37 @@
+import { Meta, Title } from '@angular/platform-browser';
+import { Observable, Subject } from 'rxjs';
+import { SeoMetaConfig } from './seo-meta-config';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
+
+@Component({
+  template: ''
+})
+export abstract class SeoComponent implements OnDestroy, OnInit {
+
+  protected onDestroy$ = new Subject<void>();
+
+  protected constructor(private meta: Meta, private title: Title) {
+  }
+
+  protected abstract getMeta$(): Observable<SeoMetaConfig>;
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
+  }
+
+  ngOnInit(): void {
+    this.getMeta$().pipe(
+      takeUntil(this.onDestroy$)
+    ).subscribe(config => {
+      this.title.setTitle(config.title);
+      this.meta.updateTag({ name: 'description', content: config.description });
+      this.meta.updateTag({ name: 'twitter:title', content: config.title });
+      this.meta.updateTag({ name: 'twitter:description', content: config.description });
+      this.meta.updateTag({ property: 'og:title', content: config.title });
+      this.meta.updateTag({ property: 'og:description', content: config.description });
+      this.meta.updateTag({ property: 'og:url', content: config.url });
+    });
+  }
+}
--- apps/guides/src/app/pages/guide/guide/guide.component.ts
+++ apps/guides/src/app/pages/guide/guide/guide.component.ts
@@ -3,17 +3,20 @@ import { combineLatest, Observable, Subject } from 'rxjs';
 import { first, map, switchMapTo, tap } from 'rxjs/operators';
 import { GuidesFacade } from '../../../database/+state/guides.facade';
 import { ActivatedRoute } from '@angular/router';
-import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import { DOCUMENT, isPlatformBrowser, Location } from '@angular/common';
 import { AuthService } from '../../../database/auth.service';
 import { Guide } from '../../../database/+state/model/guide';
 import { TableOfContentEntry } from '../../../core/guide-content/table-of-content-entry';
+import { SeoComponent } from '../../../core/seo/seo-component';
+import { Meta, Title } from '@angular/platform-browser';
+import { SeoMetaConfig } from '../../../core/seo/seo-meta-config';
 
 @Component({
   selector: 'guides-guide',
   templateUrl: './guide.component.html',
   styleUrls: ['./guide.component.less']
 })
-export class GuideComponent implements AfterContentInit {
+export class GuideComponent extends SeoComponent implements AfterContentInit {
 
   public guide$: Observable<Guide> = this.route.paramMap.pipe(
     map(params => params.get('slug')),
@@ -35,7 +38,10 @@ export class GuideComponent implements AfterContentInit {
               private authService: AuthService,
               private route: ActivatedRoute,
               @Inject(PLATFORM_ID) private platform: Object,
-              @Inject(DOCUMENT) private document: Document) {
+              @Inject(DOCUMENT) private document: Document,
+              private location: Location,
+              meta: Meta, title: Title) {
+    super(meta, title);
     this.guidesFacade.init();
   }
 
@@ -52,6 +58,18 @@ export class GuideComponent implements AfterContentInit {
         }
       });
     }
+  }
+
+  protected getMeta$(): Observable<SeoMetaConfig> {
+    return this.guide$.pipe(
+      map(guide => {
+        return {
+          title: guide.title,
+          description: guide.description,
+          url: `https://guides.ffxivteamcraft.com${this.location.path(true)}`
+        };
+      })
+    );
   }
 
 }
