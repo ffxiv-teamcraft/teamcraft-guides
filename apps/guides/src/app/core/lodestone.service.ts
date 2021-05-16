3ab8018722ed8fa7db581a6e3db098ec67d144bd
Supamiu <contact@flavien-normand.fr>
2021-05-16 14:51:43 +0200

    chore: better xivapi requests management

--- apps/guides/src/app/core/lodestone.service.ts
+++ apps/guides/src/app/core/lodestone.service.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { Character, XivapiService } from '@xivapi/angular-client';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { interval, Observable, Subject } from 'rxjs';
+import { map, shareReplay, switchMapTo } from 'rxjs/operators';
 import { firstIfServer } from './rxjs/first-if-server';
 
 @Injectable({ providedIn: 'root' })
@@ -9,17 +9,29 @@ export class LodestoneService {
 
   private cache: Record<number, Observable<Character>> = {};
 
+  private queue: Subject<void>[] = [];
+
   constructor(private xivapi: XivapiService,
               @Inject(PLATFORM_ID) private platform: Object) {
+    interval(1000).subscribe(() => {
+      const req = this.queue.shift();
+      if (req) {
+        req.next();
+      }
+    });
   }
 
   public getCharacter(id: number): Observable<Character> {
     if (!this.cache[id]) {
-      this.cache[id] = this.xivapi.getCharacter(id, { columns: ['Character.Avatar', 'Character.Name', 'Character.Title'] }).pipe(
-        map(response => response.Character),
-        shareReplay(),
-        firstIfServer(this.platform)
+      const trigger = new Subject<void>();
+      this.cache[id] = trigger.pipe(
+        switchMapTo(this.xivapi.getCharacter(id, { columns: ['Character.Avatar', 'Character.Name', 'Character.Title'] }).pipe(
+          map(response => response.Character),
+          shareReplay(),
+          firstIfServer(this.platform)
+        ))
       );
+      this.queue.push(trigger);
     }
     return this.cache[id];
   }
